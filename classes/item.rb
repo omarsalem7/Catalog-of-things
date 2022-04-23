02cf620eb223614b60a7d3ce8eab130cb0b4767d
omarsalem7 <omarsalem0721@gmail.com>
2022-04-23 17:01:06 +0200

    Add item class and can_be_archived method

--- classes/item.rb
+++ classes/item.rb
@@ -1,1 +1,13 @@
-# item class
+require 'date'
+class Item 
+    def initialize(publish_date,archived)
+        @id = Random.rand(1..1000)
+        @publish_date = Date.parse(publish_date)
+        @archived =archived
+    end
+
+    def can_be_archived?
+        Time.now.year - @publish_date.year > 10
+    end
+end
+
