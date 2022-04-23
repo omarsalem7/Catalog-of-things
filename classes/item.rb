1fb2fca0d9b13baf48d262f10a915bfaf7ad26d9
omarsalem7 <omarsalem0721@gmail.com>
2022-04-23 17:39:10 +0200

    fix linters

--- classes/item.rb
+++ classes/item.rb
@@ -1,20 +1,20 @@
 require 'date'
-class Item 
-    attr_reader :label
-    def initialize(publish_date,label,archived)
-        @id = Random.rand(1..1000)
-        @label = label
-        @publish_date = Date.parse(publish_date)
-        @archived =archived
-    end
+class Item
+  attr_reader :label
 
-    def label=(label)
-        @label=label
-        label.items.push(self) unless label.animals.include?(self)
-    end
+  def initialize(publish_date, label, archived)
+    @id = Random.rand(1..1000)
+    @label = label
+    @publish_date = Date.parse(publish_date)
+    @archived = archived
+  end
 
-    def can_be_archived?
-        Time.now.year - @publish_date.year > 10
-    end
+  def label=(label)
+    @label = label
+    label.items.push(self) unless label.animals.include?(self)
+  end
+
+  def can_be_archived?
+    Time.now.year - @publish_date.year > 10
+  end
 end
-
