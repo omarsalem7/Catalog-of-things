a9f43ec68755dd0065728841a0b7575bf22f8696
omarsalem7 <omarsalem0721@gmail.com>
2022-04-23 17:50:30 +0200

    fix minor changes

--- classes/item.rb
+++ classes/item.rb
@@ -1,20 +1,26 @@
 require 'date'
 class Item
-  attr_reader :label
+  attr_reader :id, :archived, :label
 
-  def initialize(publish_date, label, archived)
+  attr_access :publish_date
+
+  def initialize(publish_date, archived)
     @id = Random.rand(1..1000)
-    @label = label
+    @label = nil
     @publish_date = Date.parse(publish_date)
     @archived = archived
   end
 
   def label=(label)
     @label = label
-    label.items.push(self) unless label.animals.include?(self)
+    label.items.push(self) unless label.items.include?(self)
   end
 
   def can_be_archived?
     Time.now.year - @publish_date.year > 10
+  end
+
+  def move_to_archive
+    @archived = true if can_be_archived?
   end
 end
