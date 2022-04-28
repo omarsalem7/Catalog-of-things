e82aa75123c66edb3403b7658ab42263810db2f0
Yerimah <danielyerimah@yahoo.com>
2022-04-28 08:36:09 +0100

    modify genre.json file

--- classes/modules/genre_module.rb
+++ classes/modules/genre_module.rb
@@ -1,32 +1,25 @@
 require_relative '../genre'
-require 'json'
 
 class GenreModule
-  attr_accessor :genre
+  attr_accessor :genres
 
   def initialize
-    @genre = []
+    @genres = []
   end
 
   def populate_genres
-    
-    return unless @genre.empty?
+    file = File.read('./genre.json')
+    genres = JSON.parse(file, { symbolize_names: true })
+    return unless @genres.empty?
 
-    @genre.push(Genre.new('fun'))
-  end
-
-  def create_genre
-    puts 'Enter the music genre'
-    name = gets.chomp
-    new_genre = Genre.new(name)
-    @genre << new_genre
-    puts ''
-    puts 'Music genre created successfully.'
+    genres.each do |genre|
+      @genres.push(Genre.new(genre[:name]))
+    end
   end
 
   def list_all_genres
-    @genre.each_with_index do |genre, index|
-      puts "(#{index + 1}) ID: #{genre.id}, Genre Name: #{genre.name}"
+    @genres.each_with_index do |genre, index|
+      puts "(#{index + 1})  Genre Name: #{genre.name}"
     end
   end
 end
