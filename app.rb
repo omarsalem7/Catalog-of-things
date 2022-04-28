6e42c4bdd1887be769e1f959ddd28e8d81372d12
Yerimah <danielyerimah@yahoo.com>
2022-04-28 08:38:12 +0100

    modify app.rb file

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@ require './classes/modules/games_module'
 require './classes/modules/genre_module'
 require './classes/modules/music_album_module'
 require './classes/modules/author_module'
+require_relative './classes/modules/book_label_module/book_module'
+require_relative './classes/modules/book_label_module/label_module'
 
 class App
   attr_accessor :genre
@@ -11,46 +13,53 @@ class App
     @genre = GenreModule.new
     @music_album = MusicAlbumModule.new(@genre)
     @authors = AuthorModule.new
+    @labels = LabelModule.new
+    @books = BookModule.new(@labels)
   end
 
   def user_interface
-    puts ''
+    puts ' '
     puts " Please choose among the options below by pressing its corresponding number:
-                1.List all games
-                2.List all authors
-                3.Add a game
-                4.List all genres
-                5.Add genre
-                6.List all music
-                7.Add music album
-                8.Exit"
+                1.List all books
+                2.List all labels
+                3.Add a book
+                4.List all games
+                5.List all authors
+                6.Add a game
+                7.List all genres
+                8.List all music
+                9.Add music album
+                10.Exit"
+    puts ' '
   end
 
   def options
     user_interface
     selection = gets.chomp
-    @genre.populate_genres
-    case selection
-    when '1'
-      @games.list_all_games
-    when '2'
-      @authors.list_all_authors
-    when '3'
+    hash_options = { '1' => -> { @books.list_all_books }, '2' => -> { @labels.list_labels }, '3' => lambda {
+      @books.add_book
+    } }
+
+    hash_options2 = { '4' => -> { @games.list_all_games }, '5' => -> { @authors.list_all_authors }, '6' => lambda {
       @games.create_game
-    when '4'
-      @genre.list_all_genres
-    when '5'
-      @genre.create_genre
-    when '6'
+    } }
+
+    hash_options3 = { '7' => -> { @genre.list_all_genres }, '8' => lambda {
       @music_album.list_music_album
-    when '7'
-      @music_album.add_music_album
-    when '8'
-      puts 'Thank you for using the app!'
-      exit
+    } }
+
+    hash_options4 = { '9' => -> { @music_album.add_music_album }, '10' => -> { exit } }
+
+    hash_options = hash_options.merge(hash_options2, hash_options3, hash_options4)
+
+    if hash_options.key?(selection)
+      @genre.populate_genres
+      @labels.populate_labels
+      hash_options[selection].call
     else
       puts 'Invalid input. Please try again'
     end
+
     options
   end
 end
